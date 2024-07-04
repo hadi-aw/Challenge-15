f6d956588047aa2c1fb17235f8bfd1fc09668de4
Hadi Awad <hawad.abdrabbou@gmail.com>
2024-07-04 02:31:17 -0400

    Education.js
    "Display educational background in a dedicated section."

---
+++ my-app/src/components/Education.js
@@ -0,0 +1,18 @@
+// src/components/Education.js
+import React from 'react';
+import './Education.css';
+
+const Education = () => {
+    return (
+        <div className="education">
+            <h2>Education</h2>
+            <div className="education-item">
+                <h3>BS in Business Analytics & Information Systems</h3>
+                <p>University of South Florida</p>
+                <p>2022 - 2025</p>
+            </div>
+        </div>
+    );
+}
+
+export default Education;
